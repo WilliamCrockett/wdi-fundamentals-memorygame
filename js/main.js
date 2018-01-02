6a3f02f62cd34b47a166df4cde6db19fb0aff09f
William Crockett <fred@Williams-MacBook-Pro.local>
2018-01-02 10:32:04 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-console.log("Up and Running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("user flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
